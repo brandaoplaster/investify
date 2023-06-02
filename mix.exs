30679a149be45f5b2e4b70fb7a8f79f6a07f63ee
brandaoplaster <brandaoplaster@gmail.com>
2023-06-01 22:16:39 -0300

    chore: Installing credo, sobelow and coveralls

--- mix.exs
+++ mix.exs
@@ -49,7 +49,12 @@ defmodule Investify.MixProject do
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:excoveralls, "~> 0.14.4", only: :test},
+      {:sobelow, "~> 0.11.1", only: :dev},
+      {:ex_machina, "~> 2.7.0"},
+      {:faker, "~> 0.17", only: [:test, :dev]},
+      {:money, "~> 1.9"}
     ]
   end
 
